0250ed83ac6a2d55ff4b2498734f9c35bf4f07d7
stevemarcel <stevancy4@gmail.com>
2020-04-12 18:35:46 +0200

    Update findInterns.js

---
+++ js/findInterns.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+ 
+ const findDocuments = function(db, callback) {
+  const collection = db.collection('documents');
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+  };
+ 
+ module.exports = findDocuments;
