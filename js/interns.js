c9e73ddc166573f80a6a5d772fd9ee1abd464af9
stevemarcel <stevancy4@gmail.com>
2020-04-13 15:27:41 +0200

    Update interns.js

--- js/interns.js
+++ js/interns.js
@@ -1,16 +1,16 @@
 const assert = require('assert');
 
-const insertDocuments = function(db, callback) {
-  const collection = db.collection('documents');
+const insertInterns = function(db, callback) {
+  const collection = db.collection('Interns');
   collection.insertMany([
     {name : 'Steve Marcel'}, {name : 'Charles Williams'}, {name : 'Ada Smiths'}
   ], function(err, result) {
     assert.equal(err, null);
     assert.equal(3, result.result.n);
     assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the intern collection");
+    console.log("Inserted 3 Interns into the intern collection");
     callback(result);
   });
 };
 
-module.exports = insertDocuments;
+module.exports = insertInterns;
