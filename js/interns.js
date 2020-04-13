c29ca328c5cffdfca83f99f9b25df9d337b2adbc
stevemarcel <stevancy4@gmail.com>
2020-04-13 14:32:36 +0200

    Update interns.js

--- js/interns.js
+++ js/interns.js
@@ -8,7 +8,7 @@ const insertDocuments = function(db, callback) {
     assert.equal(err, null);
     assert.equal(3, result.result.n);
     assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
+    console.log("Inserted 3 documents into the intern collection");
     callback(result);
   });
 };
